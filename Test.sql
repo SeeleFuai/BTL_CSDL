0237440ee1a01d739eff1e33dfbdfe5bf978369a
NAT <88333445+SeeleFuai@users.noreply.github.com>
2025-12-23 11:42:27 +0700

    test

--- Test.sql
+++ Test.sql
@@ -1,17 +1,21 @@
 USE LinhKienDienTuQly
-SELECT HDN.MaPN, 
-    HDN.NgayNhap, 
-    NV.TenNV, 
-    NCC.TenNCC,
-    CTN.MaSP, 
-    SP.TenSP, 
-    CTN.SoLuong, 
-    CTN.GiaNhap AS 'GiaNhap'
-FROM Hoa_Don_Nhap_Hang HDN, Nhan_Vien NV, Nha_Cung_Cap NCC, ChiTiet_HD_Nhap CTN, San_Pham SP
-WHERE HDN.MaNV = NV.MaNV
-    AND HDN.MaNCC = NCC.MaNCC
-    AND HDN.MaPN = CTN.MaPN
-    AND CTN.MaSP = SP.MaSP
-    -- AND HDN.MaPN = 'PN02';
-    AND YEAR(HDN.NgayNhap) = 202;
-GO
+
+
+-- SELECT NCC.MaNCC, NCC.TenNCC
+-- FROM Nha_Cung_Cap NCC
+-- WHERE NCC.MaNCC NOT IN (
+--     SELECT HDN.MaNCC
+--     FROM Hoa_Don_Nhap_Hang HDN, ChiTiet_HD_Nhap CTN 
+--     WHERE 
+--         CTN.MaSP = 'SP02'
+--         AND HDN.MaPN = CTN.MaPN
+-- );
+
+
+SELECT NCC.MaNCC, NCC.TenNCC
+FROM Nha_Cung_Cap NCC
+WHERE NCC.MaNCC NOT IN (
+    SELECT MaNCC
+    FROM Cung_Cap
+    WHERE MaSP = 'SP02'
+);
